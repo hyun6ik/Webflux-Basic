ccd206d35312ed4b421799979e9acea902029bcd
hyun6ik <fkanwm@naver.com>
2022-09-05 02:10:32 +0900

    feat : 프로젝트 생성

---
+++ src/main/kotlin/com/hyun6ik/webfluxbasic/WebfluxBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.hyun6ik.webfluxbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxBasicApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyun6ik/webfluxbasic/WebfluxStduyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyun6ik.webfluxbasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxStduyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
